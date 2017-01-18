8f6de151cec89f55ad7bc848443fed79689ae09b
Theo GROS <tgros@e2r9p20.42.fr>
2017-01-18 12:00:55 +0100

    julia == mandelbrot ?

--- src/init_fractal.c
+++ src/init_fractal.c
@@ -6,7 +6,7 @@
 /*   By: jwalsh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/17 12:46:20 by jwalsh            #+#    #+#             */
-/*   Updated: 2017/01/18 11:49:50 by tgros            ###   ########.fr       */
+/*   Updated: 2017/01/18 12:00:24 by tgros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 static int	init_mandelbrot(t_fractal *f);
 static int	init_julia(t_fractal *f);
-
-int		julia(t_pt2 j, t_fractal *f);
 
 /*
 ** Initalizes a fractal stucture based on the parameter y.
@@ -64,11 +62,11 @@ static int	init_julia(t_fractal *f)
 		return (0);
 	f->f = &julia;
 	f->i = 300; 
-	f->c.r = -0.7;
-	f->c.i = 0.27015;
+	f->c.r = -0;
+	f->c.i = 0.000;
 	f->zoom = 1;
-	f->e.h = 400;	
-	f->e.w = 300;
+	f->e.h = 300;	
+	f->e.w = 400;
 	if(!(f->colors = ft_memalloc(sizeof(int *) * f->e.h)))
 		return (0);
 	i = -1;
--- src/julia.c
+++ src/julia.c
@@ -6,7 +6,7 @@
 /*   By: tgros <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/18 11:34:30 by tgros             #+#    #+#             */
-/*   Updated: 2017/01/18 11:45:03 by tgros            ###   ########.fr       */
+/*   Updated: 2017/01/18 11:59:47 by tgros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,16 @@
 
 int	julia(t_pt2 j, t_fractal *f)
 {
-	t_complex old;
-	t_complex new;
+	t_complex z;
 	int i;
 
-	new.r = 1.5 * (j.x - f->e.w / 2) / (0.5 * 1 * f->e.w);
-	new.i = (j.y - f->e.h / 2) / (0.5 * 1 * f->e.h);
+	z.r = 1.5 * (j.x - f->e.w / 2) / (0.5 * 1 * f->e.w);
+	z.i = (j.y - f->e.h / 2) / (0.5 * 1 * f->e.h);
 	i = 0;
-	while (i < 300 && c_modulus(new) < 2)
+	while (i < 3000 && c_modulus(z) < 2)
 	{
-		old.r = new.r;
-		old.i = new.i;
-		new.r = old.r * old.r - old.i * old.i + f->c.r;
-		new.i = 2 * old.r * old.i + f->c.i;
+		z = c_add(c_product(z, z), f->c);
 		i++;
 	}
-	return (i == 300 ? BLACK : WHITE);
+	return (i == 3000 ? BLACK : WHITE);
 }
