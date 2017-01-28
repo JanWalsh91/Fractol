ac52dbdaa554001a8487906916afa5708895deed
Theo GROS <tgros@e2r8p21.42.fr>
2017-01-28 13:24:56 +0100

    LAST PUSH.

--- include/fractol.h
+++ include/fractol.h
@@ -6,7 +6,7 @@
 /*   By: jwalsh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/16 17:03:26 by jwalsh            #+#    #+#             */
-/*   Updated: 2017/01/27 15:40:41 by jwalsh           ###   ########.fr       */
+/*   Updated: 2017/01/28 13:16:41 by tgros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,9 @@
 ** General settings
 */
 
-# define THREAD_COUNT 10
 # define WIN_COUNT 4
 # define MAX_ARG 4
-# define IMG_SIZE 100
+# define IMG_SIZE 200
 # define ZOOM 1
 # define ZOOM_POW 1.1
 # define COLORS_PER_SET 4
--- src/calc_colors.c
+++ src/calc_colors.c
@@ -6,7 +6,7 @@
 /*   By: tgros <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/17 16:44:40 by tgros             #+#    #+#             */
-/*   Updated: 2017/01/27 12:05:17 by jwalsh           ###   ########.fr       */
+/*   Updated: 2017/01/28 13:16:34 by tgros            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,23 @@ static void	*calc_colors_sections(void *v);
 
 int			calc_colors(t_fractal *f)
 {
-	pthread_t	threads[THREAD_COUNT];
+	pthread_t	threads[f->e.h];
 	int			i;
 	t_th		*th;
 	void		**ret;
 
 	i = -1;
 	ret = NULL;
-	if (!(th = (t_th *)malloc(sizeof(t_th) * THREAD_COUNT)))
+	if (!(th = (t_th *)malloc(sizeof(t_th) * f->e.h)))
 		ft_error_exit("Malloc error");
-	while (++i < THREAD_COUNT)
+	while (++i < f->e.h)
 	{
 		th[i].f = f;
 		th[i].i = i;
 		pthread_create(&threads[i], NULL, calc_colors_sections, &th[i]);
 	}
 	i = -1;
-	while (++i < THREAD_COUNT)
+	while (++i < f->e.h)
 		pthread_join(threads[i], ret);
 	return (1);
 }
@@ -48,17 +48,12 @@ int			calc_colors(t_fractal *f)
 static void	*calc_colors_sections(void *v)
 {
 	t_pt2	i;
-	int		max_x;
 	t_th	*th;
 
 	th = (t_th *)v;
-	max_x = (th->f->e.w * (th->i + 1) / THREAD_COUNT);
-	i.y = -1;
-	while (++i.y < th->f->e.h)
-	{
-		i.x = (th->f->e.w * (th->i - 1) / THREAD_COUNT);
-		while (++i.x < max_x)
-			th->f->colors[i.y][i.x] = th->f->f(i, th->f);
-	}
+	i.y = th->i;
+	i.x = -1;
+	while (++i.x < th->f->e.w)
+		th->f->colors[th->i][i.x] = th->f->f(i, th->f);
 	return (NULL);
 }
